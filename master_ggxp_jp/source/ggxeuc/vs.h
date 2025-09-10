a125b403d285be581647d86ec867cf4c0e15494e
lovenus <124386022+MoonSquaredd@users.noreply.github.com>
2025-09-10 11:50:01 -0300

    Create vs.h

---
+++ master_ggxp_jp/source/ggxeuc/vs.h
@@ -0,0 +1,17 @@
+#ifndef vs
+#define vs
+
+extern char *VsFile[3];
+
+extern int skip;
+
+extern void VsScrEnd();
+extern void VsScrInit();
+extern void VsScrLoad();
+extern void VsScrSub0();
+extern void VsScrSub1();
+extern void VsScrSub2();
+extern void VsScrSkip();
+extern void VsScrMain();
+
+#endif
